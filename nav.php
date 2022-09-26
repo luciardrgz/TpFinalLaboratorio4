2d79ef13f997afeda28221bfb37fac95bd7ab352
TomasAleman <alemanmdq@gmail.com>
2022-09-26 13:00:00 -0300

    Creacion del Nav

---
+++ nav.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="../css/nav.css">
+</head>
+<body>
+    <nav>
+	<a href="#">Home</a>
+	<a href="#">Profile</a>
+	<a href="#">Guardian&nbsp;list</a>
+	<a href="#">Booking&nbsp;history</a>
+	<a href="#">Requests</a>
+	<div class="animation start-home"></div>
+</nav>
+    
+</body>
+</html>
---
+++ view/landingPage.php
@@ -0,0 +1,18 @@
+<?php
+ namespace view;
+
+ include ("../nav.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Landpage</title>
+    <link rel="stylesheet" type="text/css" href="../css/landingPage.css">
+</head>
+<body>
+    <form action="">
+    <input type="text">
+
+    </form>
+</body>
+</html>
